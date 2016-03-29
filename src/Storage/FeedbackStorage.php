34f829665f8c62e46bb0d5aaf37043f5c6f44609
Роман Руднев <ryrudnev@gmail.com>
2016-03-30 02:17:00 +0300

    Done simple feedback storage.

--- src/Storage/FeedbackStorage.php
+++ src/Storage/FeedbackStorage.php
@@ -1,8 +1,55 @@
 <?php
 
 namespace Drupal\sfeedback\Storage;
+use Drupal\Core\Database\Database;
 
 class FeedbackStorage
 {
+    private static $_instance;
 
+    protected $tableName = 'sfeedback';
+    protected $connection;
+
+    public function getInstance()
+    {
+        return is_null(static::$_instance) ? (static::$_instance = new static()) : static::$_instance;
+    }
+
+    protected function __construct()
+    {
+        $this->connection = Database::getConnection();
+    }
+
+    private function __clone()
+    {
+
+    }
+
+    private function __wakeup()
+    {
+
+    }
+
+    public function getAll()
+    {
+        $tableName = $this->tableName;
+        return $this->connection->query("SELECT * FROM {$tableName}")->fetchAllAssoc('id');
+    }
+
+    public function exists($id)
+    {
+        $tableName = $this->tableName;
+        $result = $this->connection->query("SELECT 1 FROM {$tableName} WHERE id = :id", [':id' => $id])->fetchField();
+        return (bool) $result;
+    }
+
+    public function add(array $data)
+    {
+        return $this->connection->insert($this->tableName)->fields($data)->execute();
+    }
+
+    public function delete($id)
+    {
+        return $this->connection->delete($this->tableName)->condition('id', $id)->execute();
+    }
 }
